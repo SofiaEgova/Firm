56789e29c9def67710a7ce066f39ef8719cbdd10
Sofia <sofiakulymanova@gmail.com>
2018-11-23 13:32:35 +0400

    странички html

--- src/main/java/firm/mainController.java
+++ src/main/java/firm/mainController.java
@@ -1,9 +1,8 @@
 package firm;
 
 
-import firm.worker.Worker;
-import firm.worker.WorkerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import firm.client.Client;
+import firm.client.ClientRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,33 +10,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.util.List;
 
 
 @Controller
 public class mainController {
-    private WorkerRepository rep;
-
-//    @GetMapping("/index")
-//    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "index.html";
-//    }
+    private ClientRepository rep;
 
     @GetMapping("/index")
-    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        //Worker wr = rep.
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "index.html";
     }
 
     @PostMapping
-    public String add(@RequestParam String name,@RequestParam String post, Model model) throws IOException {
-        Worker worker = new Worker(name,post);
-        rep.save(worker);
-
-        model.addAttribute("name", name);
-        model.addAttribute("post", post);
-        return "index.html";
+    public void add(@RequestParam String name, @RequestParam String phone) throws IOException {
+        Client client = new Client();
+        client.setName(name);
+        client.setPhoneNumber(Integer.parseInt(phone));
+        rep.save(client);
     }
 }
