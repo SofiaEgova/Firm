aa9019e90f987f2368d9c21fe89d05a8f31e05b0
Sofia <sofiakulymanova@gmail.com>
2018-12-12 08:33:43 +0400

    бутстрап

--- src/main/java/firm/base/WebConfig.java
+++ src/main/java/firm/base/WebConfig.java
@@ -1,15 +1,23 @@
 package firm.base;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/resources/");
+
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/resources/public/css/");
-        registry.addResourceHandler("/image/**").addResourceLocations("/resources/public/image/");
+        registry.addResourceHandler("/public/css/**").addResourceLocations("/resources/public/css/");
+        //registry.addResourceHandler("/image/**").addResourceLocations("/resources/public/image/");
+
+//        registry
+//                .addResourceHandler("/webjars/**")
+//                .addResourceLocations("/webjars/");
     }
 }
