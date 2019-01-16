075f0bf5d63d419a5611b3c692e168c4b5a64358
Sofia <sofiakulymanova@gmail.com>
2019-01-16 13:48:11 +0400

    js с ошибками

--- src/main/java/firm/client/ClientController.java
+++ src/main/java/firm/client/ClientController.java
@@ -2,11 +2,11 @@ package firm.client;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class ClientController {
@@ -20,20 +20,20 @@ public class ClientController {
     @GetMapping("/client")
     public String client(Model model)throws IOException {
         Iterable<Client> clients = service.getAll();
-        model.addAttribute("clients",clients.toString());
+        model.addAttribute("clients",clients);
         return "client";
     }
+//
+//    @GetMapping("/getClients")
+//    public Model getClients(Model model)throws IOException {
+//        Iterable<Client> clients = service.getAll();
+//        model.addAttribute("clients",clients);
+//        return model;
+//    }
 
     @PostMapping
     public void createClient(@RequestParam String phone, @RequestParam String name) throws IOException {
         Client client = new Client(name,Integer.parseInt(phone));
         service.create(client);
-    }
-
-    @GetMapping("/clients")
-    public String main(Model model)throws IOException {
-        Iterable<Client> clients = service.getAll();
-        model.addAttribute("client","CL");
-        return "client";
     }
 }
