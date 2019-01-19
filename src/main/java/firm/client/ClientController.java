e87b2e703a25165c41cbfd30ba4b2716a04246d0
Sofia <sofiakulymanova@gmail.com>
2019-01-19 17:03:32 +0400

    js

--- src/main/java/firm/client/ClientController.java
+++ src/main/java/firm/client/ClientController.java
@@ -1,10 +1,14 @@
 package firm.client;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.enterprise.inject.Produces;
+import javax.json.Json;
+import javax.json.JsonObject;
 import java.io.IOException;
 import java.util.List;
 
@@ -23,13 +27,29 @@ public class ClientController {
         model.addAttribute("clients",clients);
         return "client";
     }
-//
-//    @GetMapping("/getClients")
-//    public Model getClients(Model model)throws IOException {
-//        Iterable<Client> clients = service.getAll();
-//        model.addAttribute("clients",clients);
-//        return model;
+
+//    @GetMapping("/client")
+//    public String client(){
+//        return "client";
 //    }
+
+//    @GetMapping("/getClients")
+//    @ResponseBody
+//    public List<Client> client()throws IOException {
+//        JSONObject obj = new JSONObject();
+//        List<Client> clients = service.getAll();
+//        for(int i=0;i<((List<Client>) clients).size();i++){
+//           obj.put(((List<Client>) clients).get(i).getId().toString(), ((List<Client>) clients).get(i).getName());
+//        }
+//        return clients;
+//    }
+
+    @GetMapping("/getClients")
+   //
+    public @ResponseBody List<Client> getClients()throws IOException {
+        List<Client> clients = service.getAll();
+        return clients;
+    }
 
     @PostMapping
     public void createClient(@RequestParam String phone, @RequestParam String name) throws IOException {
